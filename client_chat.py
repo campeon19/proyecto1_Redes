ced11da27498aa5facdddce2bc8db2a0ab21fca4
Christian Pérez <christiandp@live.com>
2022-08-08 00:22:22 -0600

    Get contact list implemented

--- client_chat.py
+++ client_chat.py
@@ -23,11 +23,41 @@ class SendMsg(slixmpp.ClientXMPP):
                           mbody=self.message, mtype='chat')
         self.disconnect()
 
+    # def send_msg(self, to, message):
+    #     self.send_message(mto=to,
+    #                       mbody=message, mtype='chat')
+    #     self.disconnect()
+
+
+class GetListContacts(slixmpp.ClientXMPP):
+    def __init__(self, jid, password):
+        slixmpp.ClientXMPP.__init__(self, jid, password)
+        self.add_event_handler("session_start", self.start)
+
+    async def start(self, event):
+        self.send_presence()
+        await self.get_roster()
+        contactos = []
+        roster = self.roster
+        for jid in roster:
+            contactos.append(jid)
+
+        # for contact in self.roster.__getattribute__('items'):
+        #     print(contact)
+        self.disconnect()
+
+        for x in range(0, 10):
+            print("")
+        print("Lista de contactos:")
+        for contact in contactos:
+            print(contact)
+
 
 def menu():
     print("""
     1. Send message
-    2. Disconnect from server
+    2. Get list contacts
+    3. Disconnect from server
     """)
 
 
@@ -51,30 +81,43 @@ if __name__ == '__main__':
                         format='%(levelname)-8s %(message)s')
 
     print('Bienvenido al chat usando el protocolo xmpp')
-    usuario = input('Ingrese su usuario completo: ')
-    password = input('Ingrese su contraseña: ')
+    # usuario = input('Ingrese su usuario completo: ')
+    # password = input('Ingrese su contraseña: ')
+
+    usuario = 'christianp@alumchat.fun'
+    password = getpass('Ingrese su contraseña: ')
 
     # client = SendMsg(usuario, password)
     # client.register_plugin('xep_0030')  # Service Discovery
     # client.register_plugin('xep_0199')  # XMPP Ping
+    # client.connect()
+    # client.process(forever=False)
 
     menu()
     opcion = input('Ingrese una opcion: ')
-    while opcion != '2':
+    while opcion != '3':
         if opcion == '1':
             to = input('Ingrese el destinatario: ')
             msg = input('Ingrese el mensaje: ')
+            # client.send_msg(to, msg)
             client = SendMsg(usuario, password, to, msg)
             client.register_plugin('xep_0030')  # Service Discovery
             client.register_plugin('xep_0199')  # XMPP Ping
             # client.disconnect()
             client.connect()
             client.process(forever=False)
+        elif opcion == '2':
+            client = GetListContacts(usuario, password)
+            client.register_plugin('xep_0030')  # Service Discovery
+            client.register_plugin('xep_0199')  # XMPP Ping
+            client.connect()
+            client.process(forever=False)
+
         menu()
         # client.disconnect()
         opcion = input('Ingrese una opcion: ')
 
-    # client.disconnect()
+    client.disconnect()
 
     print("Client disconnected.")
     exit(0)
